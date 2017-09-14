cf514e03a105c6414f38c9d9466176ee96d536c1
Dujoh Ollivierre <ollivierre.dev@gmail.com>
2017-09-14 15:31:36 -0500

    Addition of all required project files
    Added CSS folder, and JS folder. Inxe.html has now been updated.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var maxQuotes;
+var randomNumber = Math.floor( Math.random() * maxQuotes);
+
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
