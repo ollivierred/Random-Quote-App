92a06b4b9a741f2c14fbc66c2443615ff613c1e0
Dujoh Ollivierre <ollivierre.dev@gmail.com>
2017-09-14 16:05:47 -0500

    creating array for quote function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-var maxQuotes;
-var randomNumber = Math.floor( Math.random() * maxQuotes);
 
+var randomNumber = Math.floor( Math.random() * quotes.length);
+var quotes = [
+  "Effort is only effort when it begins to hurt.",
+
+  "Success is almost totally dependent upon drive and persistence. The extra energy required to make another effort or try another approach is the secret of winning.",
+
+  "In the past, those who foolishly sought power by riding on the back of the tiger ended up inside.",
+
+  "The results you achieve will be in direct proportion to the effort you apply.",
+
+  "It is easier to go down a hill than up, but the view is from the top."
+]
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
