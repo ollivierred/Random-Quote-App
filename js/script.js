b32e07b3b06e2c2539310ba1c4657256df55f137
Dujoh Ollivierre <ollivierre.dev@gmail.com>
2017-09-15 09:18:01 -0500

    Quotations being placed into Object Literals

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
 
 var randomNumber = Math.floor( Math.random() * quotes.length);
 var quotes = [
-  "Effort is only effort when it begins to hurt.",
+  {
+    quote: "Effort is only effort when it begins to hurt.",
+    source: "José Ortega y Gasset",
+    citation: "In Search of Goethe",
+    year: "1949"
+  },
+  {
+    quote: "A mind that is stretched by a new experience can never go back to its old dimensions."
+    source: "Oliver Wendell Holmes",
+    citation: "The Autocrat of the Breakfast Table",
+    year: "1858"
+  },
+  {
+    quote: "Success is almost totally dependent upon drive and persistence. The extra energy required to make another effort or try another approach is the secret of winning.",
+    source: "Denis Waitley"
+  },
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  },
 
-  "Success is almost totally dependent upon drive and persistence. The extra energy required to make another effort or try another approach is the secret of winning.",
-
-  "In the past, those who foolishly sought power by riding on the back of the tiger ended up inside.",
-
-  "The results you achieve will be in direct proportion to the effort you apply.",
-
-  "It is easier to go down a hill than up, but the view is from the top."
 ]
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
