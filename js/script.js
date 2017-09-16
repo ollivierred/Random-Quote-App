7d7e25aff139399b55615e8cd23d612ed1a91faf
Dujoh Ollivierre <ollivierre.dev@gmail.com>
2017-09-16 09:31:35 -0500

    refactored getRandomQuote function and minor syntax changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 var randQuote;
-var randQuoteSource;
-var randQuoteCitation;
-var randQuoteYear;
-var quotesAmount;
 var randQuoteNum;
 var message;
 var quotes = [
@@ -35,33 +31,38 @@ var quotes = [
     year: "1982"
   },
 ]
-
-var quotesAmount = quotes.length
+var quotesLength = quotes.length;
 function getRandNum() {
-  var randNum = Math.floor( Math.random() * quotesAmount );
+  var randNum = Math.floor( Math.random() * quotesLength );
   return randNum;
 }
 
 function getRandomQuote() {
-  for ( var i=0; i < quotesAmount; i++ ) {
+  for ( var i=0; i < quotesLength; i++ ) {
     randQuoteNum = getRandNum();
-    randQuote = quotes[randQuoteNum].quote;
-    randQuoteSource = quotes[randQuoteNum].source;
-    randQuoteCitation = quotes[randQuoteNum].citation;
-    randQuoteYear = quotes[randQuoteNum].year;
-
-    var message = '<p class="quote">'+ randQuote + '</p>' +
-                  '<p class="source">'+ randQuoteSource +
-                    '<span class="citation">'+ randQuoteCitation + '</span>' +
-                    '<span class="year">'+ randQuoteYear + '</span>' +
-                  '</p>'
+    /* In this part of the fuction, the quote and all its contents are stored in a new varible called "randQuote". Using the "getRandNum" function, a random whole number is generated, then its returned value is placed into the quotes array index. Thus creating random index numbers / random quotes.  */
+    randQuote = quotes[i];
+    // Using the dot dot object notation, the quote stored in randQuote and all its values can now be accessed.
+    if ( randQuote.citation === undefined && randQuote.year === undefined ) {
+      console.log ('Citation and year are missing.');
+    } else {
+      var message = '<p class="quote">'+ randQuote.quote + '</p>' +
+                    '<p class="source">'+ randQuote.source +
+                      '<span class="citation">'+ randQuote.citation + '</span>' +
+                      '<span class="year">'+ randQuote.year + '</span>' +
+                    '</p>';
+      console.log ('No errors present');
+    }
   }
   return message;
 }
-
+function quoteTransition () {
+  var timer = setInterval(printQuote, 5000);
+}
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById( 'loadQuote' ).addEventListener( "click", function printQuote() {
   message = getRandomQuote();
   document.querySelector('#quote-box').innerHTML = message;
+  console.log (message);
 });
