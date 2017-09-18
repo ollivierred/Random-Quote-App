b7631ad1073d1a718d0084adf13ee21d1ead7606
ollivierred <ollivierre.dev@gmail.com>
2017-09-18 01:53:55 -0500

    edits @1:53AM MON SEP 18

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-var randQuoteIndex;
+/*
+Project One: Random Quote Generator
+Course: Teamtreehouse Techdegree
+*/
+
 var quotes = [
-  // {
-  //   quote: "",
-  //   source: "",
-  //   citation: "",
-  //   year: ""
-  // },
   {
     quote: "To build a mighty tower, you must bet all of your effort and potential to it.",
     source: "Effort Anonymous",
+    citation: "",
+    year: ""
   },
   {
     quote: "There has never yet been a man in our history who led a life of ease whose name is worth remembering.",
     source: "Theodore Roosevelt",
-  },
-  {
-    quote: "Inscribe all human effort with one word, artistry's haunting curse, the Incomplete!",
-    source: "Robert Browning",
+    citation: "",
+    year: ""
   },
   {
     quote: "It is easier to go down a hill than up, but the view is from the top.",
     source: "Arnold Bennett",
+    citation: "",
+    year: ""
   },
   {
     quote: "Effort is only effort when it begins to hurt.",
@@ -35,61 +35,46 @@ var quotes = [
     year: "1858"
   },
   {
-    quote: "Success is almost totally dependent upon drive and persistence. The extra energy required to make another effort or try another approach is the secret of winning.",
-    source: "Denis Waitley"
-  },
-  {
     quote: "In the past, those who foolishly sought power by riding on the back of the tiger ended up inside.",
     source: "John F. Kennedy",
+    citation: "",
+    year: ""
   }
 ]
+
+var randQuoteIndex;
+var qLength = quotes.length;
 
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById( 'loadQuote' ).addEventListener( "click", function printQuote(){
   document.getElementById('quote-box').innerHTML = quoteString();
-  console.log ( getRandomIndex() );
 });
-
-var previousNum = []
-var quotesLength = quotes.length;
-
-//Generates the random index number
-function getRandomIndex() {
-  var randIndex = Math.floor( Math.random() * quotesLength );
-  return randIndex;
-}
 
 // This function will generate the random rbg color value, which will be used to change the background color
 function getRandomColor() {
   var red = Math.floor( Math.random() * 256 );
   var green = Math.floor( Math.random() * 256 );
   var blue = Math.floor( Math.random() * 256 );
-  // var randColor = 'rgb('+ red +',' + green+',' + blue +');';
+  var randColor = red + green + blue;
   // return randColor;
 }
 
+//Generates the random index number
+function getRandomIndex() {
+  return Math.floor( Math.random() * qLength );
+}
+
 function getRandomQuote() {
-  for ( var i=0; i < quotesLength; i++ ) {
-    randQuoteIndex = getRandomIndex();
-    /* In this part of the fuction, the quote and all its contents are stored in a new varible called "randQuote". Using the "getRandNum" function, a random whole number is generated, then its returned value is placed into the quotes array index. Thus creating random index numbers / random quotes.  */
-    randQuote = quotes[randQuoteIndex];
-    return randQuote;
-    // Using the dot object notation, the quote stored in randQuote and all its values can now be accessed.
-    }
+  var qIndex = getRandomIndex();
+  randIndex = quotes[qIndex];
+  return randIndex;
 }
 
 //Assembles the contents of the stored quote into a string, and stores it in message. Also checkes if the qoute has citation and year.
 function quoteString () {
-  var randQuote = getRandomQuote();
-  if ( randQuote.citation === undefined || randQuote.year === undefined ) {
-    return message = '<p class="quote">'+ randQuote.quote + '</p>' + '<p class="source">'+ randQuote.source + '</p>';
-  } else {
-    return message = '<p class="quote">'+ randQuote.quote + '</p>' +
-                  '<p class="source">'+ randQuote.source +
-                    '<span class="citation">'+ randQuote.citation + '</span>' +
-                    '<span class="year">'+ randQuote.year + '</span>' +
-                  '</p>';
-  }
-
+  var newIndex = getRandomQuote();
+  var qString =  '<p class="quote">'+ newIndex.quote + '</p>' +
+                    '<p class="source">'+ newIndex.source + '<span class="citation">'+ newIndex.citation + '</span><span class="year">'+ newIndex.year + '</span></p>';
+  return qString;
 }
